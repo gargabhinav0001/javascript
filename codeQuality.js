4358e93edf7110255f5ac877425dd87ef784bed7
gargabhinav0001 <61727750+gargabhinav0001@users.noreply.github.com>
2022-04-12 14:24:20 +0530

    Update codeQuality.js
    Changes-2 made by Abhinav Garg

--- codeQuality.js
+++ codeQuality.js
@@ -1,1 +1,25 @@
 // level {1/2/3}
+async function getUsers() {
+    let userProfile = {
+        user: “”,
+        profile: “”,
+        posts: “”
+    };
+
+    await Promise.all(getUser(), getProfile(), getPosts())
+        .then(
+            res => {
+                userProfile = {
+                    user: res[0],
+                    profile: res[1],
+                    posts: res[2]
+                };
+
+            }
+        )
+        .catch(
+            error => {
+                console.log(error);
+            }
+        )
+}
